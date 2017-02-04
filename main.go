e35f7bf74d5f844834d6893cbbe3137c7e21201c
Ruellia Zhang <syncyes@gmail.com>
2017-02-03 22:43:52 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+type DataStruct struct {
+	MinPrice int
+	MaxPrice int
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	s := aggregateData(r, w)
+	fmt.Fprint(w, s.MinPrice+s.MaxPrice)
+	cmd := exec.Command("/bin/sh", "dockerize")
+	if err := cmd.Run(); err != nil {
+		fmt.Fprint(w, "an error has occurred")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func aggregateData(r *http.Request, w http.ResponseWriter) DataStruct {
+	min, err := strconv.ParseInt(r.FormValue("min_price"), 10, 64)
+	if err != nil {
+		return DataStruct{}
+	}
+	max, err := strconv.ParseInt(r.FormValue("max_price"), 10, 64)
+	if err != nil {
+		return DataStruct{}
+	}
+	return DataStruct{
+		MinPrice: int(min),
+		MaxPrice: int(max),
+	}
+}
