7874873f84efcf3ba5b8ac310b5c1d806ae6e025
Ruellia Zhang <syncyes@gmail.com>
2017-02-19 20:23:00 -0800

    super gross long docker command

--- main.go
+++ main.go
@@ -4,26 +4,30 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
-	"strconv"
 )
 
-const dockerImage = "personal-bot"
+const dockerImage = "bathrooms"
 
 type DataStruct struct {
-	MinPrice int
-	MaxPrice int
+	MinPrice   string
+	MaxPrice   string
 	SlackToken string
+	Bedrooms   string
+	Bathrooms  string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("receiving at server...")
 	fmt.Fprint(w, "receiving a request...")
 	s := aggregateData(r, w)
-	fmt.Fprint(w, s.MinPrice+s.MaxPrice)
 	// cmd := exec.Command("docker", "run", "hello-world")
-	slackToken := "SLACK_TOKEN="+ s.SlackToken
+	slackToken := "SLACK_TOKEN=" + s.SlackToken
+	minPrice := "MIN_PRICE=" + s.MinPrice
+	maxPrice := "MAX_PRICE=" + s.MaxPrice
+	bed := "BEDROOMS=" + s.Bedrooms
+	bath := "BATHROOMS=" + s.Bathrooms
 	cmd := "docker"
-	cmdArgs := []string{"run", "-d", "-e", slackToken, dockerImage}
+	cmdArgs := []string{"run", "-d", "-e", slackToken, minPrice, maxPrice, bed, bath, dockerImage}
 	out, err := exec.Command(cmd, cmdArgs...).Output()
 	if err != nil {
 		fmt.Fprint(w, "an error has occurred")
@@ -37,18 +41,33 @@ func main() {
 }
 
 func aggregateData(r *http.Request, w http.ResponseWriter) DataStruct {
-	min, err := strconv.ParseInt(r.FormValue("min_price"), 10, 64)
-	if err != nil {
-		return DataStruct{}
-	}
-	max, err := strconv.ParseInt(r.FormValue("max_price"), 10, 64)
-	if err != nil {
-		return DataStruct{}
-	}
+	// min, err := strconv.ParseInt(r.FormValue("min_price"), 10, 64)
+	// if err != nil {
+	// 	return DataStruct{}
+	// }
+	// max, err := strconv.ParseInt(r.FormValue("max_price"), 10, 64)
+	// if err != nil {
+	// 	return DataStruct{}
+	// }
+	// bath, err := strconv.ParseInt(r.FormValue("bathrooms"), 10, 64)
+	// if err != nil {
+	// 	return DataStruct{}
+	// }
+	// bed, err := strconv.ParseInt(r.FormValue("bedrooms"), 10, 64)
+	// if err != nil {
+	// 	return DataStruct{}
+	// }
 	token := r.FormValue("slack_token")
+	min := r.FormValue("min_price")
+	max := r.FormValue("max_price")
+	bath := r.FormValue("bathrooms")
+	bed := r.FormValue("bedrooms")
+
 	return DataStruct{
-		MinPrice: int(min),
-		MaxPrice: int(max),
+		MinPrice:   min,
+		MaxPrice:   max,
 		SlackToken: token,
+		Bedrooms:   bed,
+		Bathrooms:  bath,
 	}
 }
