69b0596b01a490eaa524402b5d0e12db6de38615
Ruellia Zhang <syncyes@gmail.com>
2017-03-15 02:55:31 -0700

    refactor

--- main.go
+++ main.go
@@ -2,39 +2,27 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
-	"path/filepath"
+
+	"github.com/ruellia/quikrent-bash/settings"
 
 	_ "github.com/go-sql-driver/mysql"
-	uuid "github.com/satori/go.uuid"
 )
 
 const dockerImage = "please_work"
 
-var db *sql.DB
-
-type CraigslistSettings struct {
-	MinPrice        float64                `json:"min_price"`
-	MaxPrice        float64                `json:"max_price"`
-	SlackToken      string                 `json:"slack_token"`
-	Bedrooms        float64                `json:"bed"`
-	Bathrooms       float64                `json:"bath"`
-	Neighborhoods   []string               `json:"neighborhoods"`
-	TransitStations map[string]interface{} `json:"transit_stations"`
-	AbsolutePath    string
+// RootHandler is a handler that deals with requests to the root directory.
+type RootHandler struct {
+	db *sql.DB
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// IMPORTANT!!!
+func (handler *RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	// settings is request converted to our settings struct
-	settings, err := convertJSONRequest(r)
+	s, err := settings.ConvertJSONRequest(r)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -42,21 +30,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	var test string
-	err = db.QueryRow("SELECT slack_token FROM docker WHERE slack_token=?", settings.SlackToken).Scan(&test)
+	err = handler.db.QueryRow("SELECT slack_token FROM docker WHERE slack_token=?", s.BotSettings.SlackToken).Scan(&test)
 	if err == sql.ErrNoRows {
-		if err := createJSONFile(&settings); err != nil {
+		if err := settings.CreateJSONFile(&s); err != nil {
 			fmt.Fprint(w, err.Error())
 			return
 		}
 		cmd := "docker"
-		jsonSettings := "JSON_SETTINGS=" + settings.AbsolutePath
-		cmdArgs := []string{"run", "-d", "-e", jsonSettings, "-v", settings.AbsolutePath + ":" + settings.AbsolutePath + ":ro", dockerImage}
+		jsonSettings := "JSON_SETTINGS=" + s.AbsolutePath
+		cmdArgs := []string{"run", "-d", "-e", jsonSettings, "-v", s.AbsolutePath + ":" + s.AbsolutePath + ":ro", dockerImage}
 		out, err := exec.Command(cmd, cmdArgs...).Output()
 		if err != nil {
 			http.Error(w, "docker error: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-		if err := insertNewRow(settings, string(out[:])); err != nil {
+		if err := insertNewRow(handler.db, s, string(out[:])); err != nil {
 			http.Error(w, "database error: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -67,53 +55,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// let's try to db stuff
-	var err error
-	db, err = sql.Open("mysql",
+	db, err := sql.Open("mysql",
 		"root:password@tcp(127.0.0.1:3306)/quikrent")
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
-		fmt.Print("database issues")
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", handler)
+	http.Handle("/", &RootHandler{db: db})
 	http.ListenAndServe(":8080", nil)
 }
 
-func convertJSONRequest(r *http.Request) (CraigslistSettings, error) {
-	decoder := json.NewDecoder(r.Body)
-	var converted CraigslistSettings
-	if err := decoder.Decode(&converted); err != nil {
-		return CraigslistSettings{}, err
-	}
-	return converted, nil
-}
-
 // not the best name i think since it does more than just insert...rename later?
-func insertNewRow(settings CraigslistSettings, containerID string) error {
-	_, err := db.Exec("INSERT INTO docker(slack_token, container_id, json_path) VALUES(?, ?, ?)", settings.SlackToken, containerID, settings.AbsolutePath)
+func insertNewRow(db *sql.DB, settings settings.Settings, containerID string) error {
+	_, err := db.Exec("INSERT INTO docker(slack_token, container_id, json_path) VALUES(?, ?, ?)", settings.BotSettings.SlackToken, containerID, settings.AbsolutePath)
 	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func createJSONFile(settings *CraigslistSettings) error {
-	// marshal the struct into a json byte array?
-	marshaled, err := json.Marshal(settings)
-	if err != nil {
-		return err
-	}
-	// generate uuid as json file name?
-	fileName := uuid.NewV4().String() + ".json" // <- need to also store this in db
-	fileName, _ = filepath.Abs("../" + fileName)
-	settings.AbsolutePath = fileName
-	if err := ioutil.WriteFile(fileName, marshaled, 0644); err != nil {
 		return err
 	}
 	return nil
---
+++ settings/settings.go
@@ -0,0 +1,57 @@
+package settings
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+
+	"github.com/ruellia/quikrent-bash/util"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// Settings are the settings used to query the database.
+type Settings struct {
+	BotSettings  BotSettings
+	AbsolutePath string
+}
+
+// BotSettings are the settings that will be passed into the Slackbot.
+type BotSettings struct {
+	MinPrice        float64                `json:"min_price"`
+	MaxPrice        float64                `json:"max_price"`
+	SlackToken      string                 `json:"slack_token"`
+	Bedrooms        float64                `json:"bed"`
+	Bathrooms       float64                `json:"bath"`
+	Neighborhoods   []string               `json:"neighborhoods"`
+	TransitStations map[string]interface{} `json:"transit_stations"`
+}
+
+// CreateJSONFile creates a JSON file based on the BotSettings struct.
+func CreateJSONFile(settings *Settings) error {
+	marshaled, err := json.Marshal(settings.BotSettings)
+	if err != nil {
+		return err
+	}
+	fileName := uuid.NewV4().String() + ".json"
+	fileName, err = filepath.Abs(util.UserHomeDir() + "/" + fileName)
+	if err != nil {
+		return err
+	}
+	settings.AbsolutePath = fileName
+	if err := ioutil.WriteFile(fileName, marshaled, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ConvertJSONRequest converts a JSON HTTP request into our Settings struct.
+func ConvertJSONRequest(r *http.Request) (Settings, error) {
+	decoder := json.NewDecoder(r.Body)
+	var converted BotSettings
+	if err := decoder.Decode(&converted); err != nil {
+		return Settings{}, err
+	}
+	return Settings{BotSettings: converted}, nil
+}
